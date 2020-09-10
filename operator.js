968f2b6fe2a35c9580da8f2408a99d04c601b39a
Yumibk82 <70868707+Yumibk82@users.noreply.github.com>
2020-09-10 13:47:23 +0700

    thuc_hanh

---
+++ operator.js
@@ -0,0 +1,17 @@
+let a=5;
+let b=10;
+let plus=a+b;
+document.write(plus);
+
+
+let a=10;
+let b="10";
+let str1="Hung"
+let promptValue= prompt("nhap data");
+let convert=parseInt(a);
+a=convert;
+let s=str1+a;
+let compare= a===b;
+// let compare2 =a!==b && a%b==00;
+document.write(compare);
+
